cf110b585adeebf6f2544a5abfe01087db0f4246
Timothy Rorie Jr <rorie.timothyjr@gmail.com>
2021-01-11 06:37:44 -0500

    Add notes for Message Quote Skeletons, MadLib Examples

---
+++ main.js
@@ -0,0 +1,21 @@
+const messages = [];
+
+const msgSkeletons = [];
+/*
+
+Quote Mad Libs
+
+"When you reach the end of your rope, tie a knot in it and hang on." -Franklin D. Roosevelt
+"Spread love everywhere you go. Let no one ever come to you without leaving happier." -Mother Teresa
+"Don't judge each day by the harvest you reap but by the seeds that you plant." -Robert Louis Stevenson
+"Tell me and I forget. Teach me and I remember. Involve me and I learn." -Benjamin Franklin
+"The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart." -Helen Keller
+"It is during our darkest moments that we must focus to see the light." -Aristotle
+"Do not go where the path may lead, go instead where there is no path and leave a trail." -Ralph Waldo Emerson
+"The greatest glory in living lies not in never falling, but in rising every time we fall." -Nelson Mandela 
+"Many of life's failures are people who did not realize how close they were to success when they gave up." -Thomas A. Edison
+"You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose." -Dr. Seuss
+"Mother. I crave violence. -Cats"
+"The only impossible journey is the one you never begin." -Tony Robbins
+Give a man a fish, and you feed him for a day. Teach a man to fish, and you feed him for a lifetime. -Lao-Tzu, Anne Isabella Thackeray Ritchie, maybe someone else... pronbably
+*/
